ac49a51fa43f4537a6328a82c120cdb0d6688a30
raulgomez <raulgombru@gmail.com>
2017-12-13 15:55:02 +0100

    first

---
+++ customDataset.py
@@ -0,0 +1,76 @@
+from __future__ import print_function, division
+import os
+import torch
+import pandas as pd
+from skimage import io, transform
+import numpy as np
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+import cv2
+import customTransform
+from PIL import Image
+
+class CustomDataset(Dataset):
+    """Face Landmarks dataset."""
+
+    def __init__(self, root_dir, split, Rescale, RandomCrop, Mirror):
+        """
+        Args:
+            csv_file (string): Path to the csv file with annotations.
+            root_dir (string): Directory with all the images.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.root_dir = root_dir
+        self.Rescale = Rescale
+        self.RandomCrop = RandomCrop
+        self.Mirror = Mirror
+
+
+        split_f  = '{}/{}.txt'.format(root_dir, split)
+        self.indices = open(split_f, 'r').read().splitlines()
+
+        # Save image labels (one hot encoding)
+        self.labels = [int(i.split(',', 1)[1]) for i in self.indices]
+        # self.labels_onehot = []
+        # for l in labels:
+        #     onehot = np.zeros(10)
+        #     onehot[l] = 1
+        #     self.labels_onehot.append(onehot)
+
+        # Save image names
+        self.indices = [i.split(',', 1)[0] for i in self.indices]
+
+    def __len__(self):
+        return len(self.indices)
+
+
+    def __getitem__(self, idx):
+        img_name = self.root_dir + '/img_resized_1M/cities_instagram/' + self.indices[idx] + '.jpg'
+        try:
+            image = Image.open(img_name)
+            # print("Img FOUND")
+        except:
+            # print("Img file not found")
+            img_name = self.root_dir + '/img_resized_1M/cities_instagram/london/1481255189662056249.jpg'
+            image = Image.open(img_name)
+
+        if self.Rescale != 0:
+            image = customTransform.Rescale(image,self.Rescale)
+
+        if self.RandomCrop != 0:
+            image = customTransform.RandomCrop(image,self.RandomCrop)
+
+        if self.Mirror:
+            image = customTransform.Mirror(image)
+
+        im_np = np.array(image, dtype=np.float32)
+        im_np = customTransform.PreprocessImage(im_np)
+        out_img = np.copy(im_np)
+
+        # label = torch.from_numpy(self.labels_onehot[idx])
+        label = torch.from_numpy(np.array([int(self.labels[idx])]))
+        label = label.type(torch.LongTensor)
+
+        return torch.from_numpy(out_img), label
---
+++ main.py
@@ -0,0 +1,289 @@
+import os
+import shutil
+import time
+
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.distributed as dist
+import torch.optim
+import torch.utils.data
+import torch.utils.data.distributed
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torchvision.models as models
+import customDataset
+
+# model_names = sorted(name for name in models.__dict__
+#     if name.islower() and not name.startswith("__")
+#     and callable(models.__dict__[name]))
+
+dataset = '../../datasets/SocialMedia' # Path to dataset
+split_train = 'city_classification_gt/cities_instagram/trainCitiesClassification'
+split_val =  'city_classification_gt/cities_instagram/valCitiesClassification'
+arch = 'resnet18'
+workers = 4 # Num of data loading workers
+epochs = 90
+start_epoch = 0 # Useful on restarts
+batch_size = 12 #256 # Batch size
+lr = 0.1 # Initial learning rate
+momentum = 0.9
+weight_decay = 1e-4
+print_freq = 10
+resume = None # Path to checkpoint top resume training
+pretrained = False
+evaluate = True # Evaluate model on validation set
+world_size = 1 # Number of distributed processes
+dist_url = 'tcp://224.66.41.62:23456'
+dist_backend = 'gloo'
+
+def main():
+
+    distributed = world_size > 1
+
+    if distributed:
+        dist.init_process_group(backend=dist_backend, init_method=dist_url,
+                                world_size=world_size)
+
+    # create model
+    if pretrained:
+        print("=> using pre-trained model '{}'".format(arch))
+        model = models.__dict__[arch](pretrained=True)
+    else:
+        print("=> creating model '{}'".format(arch))
+        model = models.__dict__[arch]()
+
+    if not distributed:
+        if arch.startswith('alexnet') or arch.startswith('vgg'):
+            model.features = torch.nn.DataParallel(model.features)
+            model.cuda()
+        else:
+            model = torch.nn.DataParallel(model).cuda()
+    else:
+        model.cuda()
+        model = torch.nn.parallel.DistributedDataParallel(model)
+
+    # define loss function (criterion) and optimizer
+    criterion = nn.CrossEntropyLoss().cuda()
+
+    optimizer = torch.optim.SGD(model.parameters(), lr,
+                                momentum=momentum,
+                                weight_decay=weight_decay)
+
+    # optionally resume from a checkpoint
+    if resume:
+        if os.path.isfile(resume):
+            print("=> loading checkpoint '{}'".format(resume))
+            checkpoint = torch.load(resume)
+            start_epoch = checkpoint['epoch']
+            best_prec1 = checkpoint['best_prec1']
+            model.load_state_dict(checkpoint['state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(resume, checkpoint['epoch']))
+        else:
+            print("=> no checkpoint found at '{}'".format(resume))
+
+    cudnn.benchmark = True
+
+    try_dataset = datasets.ImageFolder(
+        '/home/raulgomez/imsample/',
+        transforms.Compose([
+            transforms.RandomCrop(224),
+            transforms.ToTensor()
+        ]))
+
+    # Data loading code
+    train_dataset = customDataset.CustomDataset(
+        dataset,split_train,Rescale=0,RandomCrop=224,Mirror=True)
+
+    val_dataset = customDataset.CustomDataset(
+        dataset, split_val,Rescale=224,RandomCrop=0,Mirror=False)
+
+    if distributed:
+        train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
+    else:
+        train_sampler = None
+
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=batch_size, shuffle=(train_sampler is None),
+        num_workers=workers, pin_memory=True, sampler=train_sampler)
+
+    val_loader = torch.utils.data.DataLoader(
+        val_dataset, batch_size=batch_size, shuffle=False, num_workers=workers, pin_memory=True)
+
+    val_loader_b = torch.utils.data.DataLoader(
+        try_dataset, batch_size=batch_size, shuffle=False, num_workers=workers, pin_memory=True)
+
+    if evaluate:
+        validate(val_loader_b, model, criterion)
+        validate(val_loader, model, criterion)
+        return
+
+    for epoch in range(start_epoch, epochs):
+        if distributed:
+            train_sampler.set_epoch(epoch)
+        adjust_learning_rate(optimizer, epoch)
+
+        # train for one epoch
+        train(train_loader, model, criterion, optimizer, epoch)
+
+        # evaluate on validation set
+        prec1 = validate(val_loader, model, criterion)
+
+        # remember best prec@1 and save checkpoint
+        is_best = prec1 > best_prec1
+        best_prec1 = max(prec1, best_prec1)
+        save_checkpoint({
+            'epoch': epoch + 1,
+            'arch': arch,
+            'state_dict': model.state_dict(),
+            'best_prec1': best_prec1,
+            'optimizer' : optimizer.state_dict(),
+        }, is_best)
+
+
+def train(train_loader, model, criterion, optimizer, epoch):
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    # switch to train mode
+    model.train()
+
+    end = time.time()
+    for i, (input, target) in enumerate(train_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        target = target.cuda(async=True)
+        input_var = torch.autograd.Variable(input)
+        target_var = torch.autograd.Variable(target)
+
+        # compute output
+        output = model(input_var)
+        loss = criterion(output, target_var)
+
+        # measure accuracy and record loss
+        prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
+        losses.update(loss.data[0], input.size(0))
+        top1.update(prec1[0], input.size(0))
+        top5.update(prec5[0], input.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % print_freq == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                   epoch, i, len(train_loader), batch_time=batch_time,
+                   data_time=data_time, loss=losses, top1=top1, top5=top5))
+
+
+def validate(val_loader, model, criterion):
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    # switch to evaluate mode
+    model.eval()
+
+    end = time.time()
+    for i, (input, target) in enumerate(val_loader):
+        target = target.cuda(async=True)
+        input_var = torch.autograd.Variable(input, volatile=True)
+        target_var = torch.autograd.Variable(target, volatile=True)
+
+        # compute output
+        output = model(input_var)
+        loss = criterion(output, target_var)
+
+        # measure accuracy and record loss
+        prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
+        losses.update(loss.data[0], input.size(0))
+        top1.update(prec1[0], input.size(0))
+        top5.update(prec5[0], input.size(0))
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % print_freq == 0:
+            print('Test: [{0}/{1}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                   i, len(val_loader), batch_time=batch_time, loss=losses,
+                   top1=top1, top5=top5))
+
+    print(' * Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f}'
+          .format(top1=top1, top5=top5))
+
+    return top1.avg
+
+
+def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, 'model_best.pth.tar')
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def adjust_learning_rate(optimizer, epoch):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    lr = lr * (0.1 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
+
+
+if __name__ == '__main__':
+    main()
+
